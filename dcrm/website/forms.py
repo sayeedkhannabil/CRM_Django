e3a7131fb909b80a1b39803ffda55e7acc4957f1
sayeedkhannabil <sayeedkhannabil@gmail.com>
2023-10-29 14:49:36 -0500

    project structure done

--- dcrm/website/forms.py
+++ dcrm/website/forms.py
@@ -2,6 +2,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from .models import Record
 
 # Define a custom user registration form that inherits from UserCreationForm.
 class SignupForm(UserCreationForm):
@@ -32,3 +33,20 @@ class SignupForm(UserCreationForm):
         self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
         self.fields['password2'].label = ''
         self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'
+
+
+# CREATE ADD RECORD FORM
+
+class AddRecordForm(forms.ModelForm):
+    first_name = forms.CharField(required=True, widget=forms.TextInput(attrs={"placeholder": "First Name", "class": "form-control"}), label="")
+    last_name = forms.CharField(required=True, widget=forms.TextInput(attrs={"placeholder": "Last Name", "class": "form-control"}), label="")
+    email = forms.CharField(required=True, widget=forms.TextInput(attrs={"placeholder": "Email", "class": "form-control"}), label="")
+    phone = forms.CharField(required=True, widget=forms.TextInput(attrs={"placeholder": "Phone", "class": "form-control"}), label="")
+    address = forms.CharField(required=True, widget=forms.TextInput(attrs={"placeholder": "Address", "class": "form-control"}), label="")
+    city = forms.CharField(required=True, widget=forms.TextInput(attrs={"placeholder": "City", "class": "form-control"}), label="")
+    state = forms.CharField(required=True, widget=forms.TextInput(attrs={"placeholder": "State", "class": "form-control"}), label="")
+    zip_code = forms.CharField(required=True, widget=forms.TextInput(attrs={"placeholder": "Zip Code", "class": "form-control"}), label="")
+
+    class Meta:
+        model = Record
+        exclude = ("user",)
--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignupForm
+from .forms import SignupForm, AddRecordForm
 from .models import Record
 
 def home(request):
@@ -45,3 +45,50 @@ def register_user(request):
         return render(request, 'register.html', {'form':form})
     
     return render(request, 'register.html', {'form':form})
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        # Look up the record by primary key (pk) and pass it to the template.
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record':customer_record})
+    else:
+        messages.success(request, ('Please log in to view user records.'))
+        return redirect('home')
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, ('User Record Has Been Deleted!'))
+        return redirect('home')
+    
+    else:
+        messages.success(request, ('Please log in to delete user records.'))
+        return redirect('home')
+    
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, ('Record Has Been Added!'))
+                return redirect('home')
+        return render(request, 'add_record.html', {'form':form})
+    else:
+        messages.success(request, ('Please log in to add user records.'))
+        return redirect('home')
+    
+
+def update_record(request, pk):
+	if request.user.is_authenticated:
+		current_record = Record.objects.get(id=pk)
+		form = AddRecordForm(request.POST or None, instance=current_record)
+		if form.is_valid():
+			form.save()
+			messages.success(request, "Record Has Been Updated!")
+			return redirect('home')
+		return render(request, 'update_record.html', {'form':form})
+	else:
+		messages.success(request, "You Must Be Logged In...")
+		return redirect('home')
