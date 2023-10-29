24d43f5e80dc7e24a7287fb914faf7e6554cd188
sayeedkhannabil <sayeedkhannabil@gmail.com>
2023-10-29 11:50:22 -0500

    basic database connection and schema done

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,4 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import SignupForm
+from .models import Record
 
 def home(request):
-    return render(request, 'home.html', {})
+    records = Record.objects.all()
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # Authenticate Person
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            # Login Person
+            login(request, user)
+            messages.success(request, ('You have been logged in!'))
+            return redirect('home')
+        else:
+            messages.success(request, ('Error Logging In - Please Try Again...'))
+            return redirect('home')
+
+    return render(request, 'home.html', {'records':records})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ('You have been logged out!'))
+    return redirect('home')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and Login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            messages.success(request, ('You have been registered!'))
+            return redirect('home')
+    else:
+        form = SignupForm()
+        return render(request, 'register.html', {'form':form})
+    
+    return render(request, 'register.html', {'form':form})
